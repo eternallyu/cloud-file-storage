c1849fe8d162309748ee25913d0baceeeb9dc951
eternallyu <zenq7q@gmail.com>
2025-05-31 11:02:33 +0400

    More methods in MinioRepository

--- src/main/java/ru/eternallyu/cloudfilestorage/repository/MinioRepository.java
+++ src/main/java/ru/eternallyu/cloudfilestorage/repository/MinioRepository.java
@@ -1,6 +1,7 @@
 package ru.eternallyu.cloudfilestorage.repository;
 
 import io.minio.*;
+import io.minio.messages.Item;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
@@ -12,8 +13,11 @@ import ru.eternallyu.cloudfilestorage.entity.User;
 import ru.eternallyu.cloudfilestorage.error.StorageException;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Repository
 @RequiredArgsConstructor
@@ -115,8 +119,105 @@ public class MinioRepository {
         }
     }
 
+    public Iterable<Result<Item>> getDirectoriesResources(String prefix) {
+        return minioClient.listObjects(
+                ListObjectsArgs.builder()
+                        .bucket(minioProperties.getBucket())
+                        .prefix(prefix)
+                        .recursive(false)
+                        .build()
+        );
+    }
 
+    public void renameFile(String oldPath, String newPath) {
+        try {
+            minioClient.copyObject(
+                    CopyObjectArgs.builder()
+                            .source(CopySource.builder()
+                                    .bucket(minioProperties.getBucket())
+                                    .object(oldPath)
+                                    .build())
+                            .bucket(minioProperties.getBucket())
+                            .object(newPath)
+                            .build()
+            );
+        } catch (Exception exception) {
+            throw new StorageException("Error during rename file: " + exception.getMessage());
+        }
+        deleteFile(oldPath);
+    }
 
+    public void renameFolder(String oldPath, String newPath) {
+        try {
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(minioProperties.getBucket())
+                            .prefix(oldPath).recursive(true)
+                            .build()
+            );
+
+            for (Result<Item> result : results) {
+                Item item = result.get();
+                String oldObjectName = item.objectName();
+                String newObjectName = oldObjectName.replaceFirst(oldPath, newPath);
+
+                renameFile(oldObjectName, newObjectName);
+
+            }
+        } catch (Exception exception) {
+            throw new StorageException("Error during rename folder: " + exception.getMessage());
+        }
+    }
+
+    public InputStreamResource downloadFolder(String path) {
+        try {
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(minioProperties.getBucket())
+                            .prefix(path)
+                            .recursive(true)
+                            .build()
+            );
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            try (ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
+                for (Result<Item> result : results) {
+                    Item item = result.get();
+                    String fullObjectName = item.objectName();
+                    String relativeObjectName = fullObjectName.substring(path.length());
+
+                    if (fullObjectName.endsWith("/")) {
+                        ZipEntry zipEntry = new ZipEntry(relativeObjectName);
+                        zipOutputStream.putNextEntry(zipEntry);
+                        zipOutputStream.closeEntry();
+                    } else {
+                        ZipEntry entry = new ZipEntry(relativeObjectName);
+                        zipOutputStream.putNextEntry(entry);
+
+                        try (InputStream is = minioClient.getObject(
+                                GetObjectArgs.builder()
+                                        .bucket(minioProperties.getBucket())
+                                        .object(fullObjectName)
+                                        .build()
+                        )) {
+                            byte[] buffer = new byte[8192];
+                            int len;
+                            while ((len = is.read(buffer)) > 0) {
+                                zipOutputStream.write(buffer, 0, len);
+                            }
+                        }
+                        zipOutputStream.closeEntry();
+                    }
+                }
+            }
+            byte[] zipBytes = byteArrayOutputStream.toByteArray();
+
+            return new InputStreamResource(new ByteArrayInputStream(zipBytes));
+        } catch (Exception exception) {
+            throw new StorageException("Error during download folder: " + exception.getMessage());
+        }
+    }
 
     private String getUserRootFolderName(String name) {
         Optional<User> user = userRepository.findByUsername(name);
